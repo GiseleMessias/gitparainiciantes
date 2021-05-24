21a902b25556ea24259ae06de4e38e44a9dbd767
Gisele de Castro Faustino <giselecf@primeit.com.br>
2021-05-24 12:12:42 -0300

    Iniciando nosso projeto

---
+++ ASerChamada.java
@@ -0,0 +1,6 @@
+public class ASerChamada{
+
+    public void testePrint(){
+        System.out.println("teste 2 ");
+    }
+}
---
+++ TesteGi.java
@@ -0,0 +1,9 @@
+public class TesteGi{
+    public static void main (String[] args){
+         System.out.println("Segunda chamada dos testes, alterando no GitHub");
+
+         ASerChamada saida = new ASerChamada();
+
+         saida.testePrint();
+    }
+}
